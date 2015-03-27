c703fae56c1b9d5ddee82118826d11e162b209b7
Alexandre Busquets Triola <alex@sigte.udg.edu>
2015-03-27 17:12:22 +0100

    Canvis

--- workspace/ejercicios/ejercicios_04.py
+++ workspace/ejercicios/ejercicios_04.py
@@ -5,6 +5,8 @@ Ejercicio 4:
 
 Aplicad una paleta de color específica para la representación de capas NDVI
 r.colors map=NDVI color=ndvi
+
+Este no hace falta
 """
 print "Ejecutando %s" % __file__
 
@@ -13,4 +15,5 @@ import grass.script as grass
 
 ##EJERCICIO## ##EJECUTAR_COMANDO## ##APLICAR PALETA DE COLOR## ##PARÁMETROS##
 
+#No hace falta
 
